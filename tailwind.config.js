51f9d03e957ccdd01b347691438c88033f92be87
Ema0502 <ema050296@gmail.com>
2025-05-31 20:39:30 -0300

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     "./projects/shared/src/lib/**/*.{html,ts,scss}",
     "./projects/ui-documentation/src/**/*.{html,ts,scss}"
   ],
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
