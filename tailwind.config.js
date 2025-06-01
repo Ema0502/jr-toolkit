b009b78fc1941c902f5bb172ca268acd8db05d5f
Ema0502 <ema050296@gmail.com>
2025-06-01 19:49:49 -0300

    fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./projects/ui-documentation/src/**/*.{html,ts,scss}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          50:  "#f9f9fa",
+          100: "#ececec",
+          200: "#d9d9d9",
+          300: "#bfbfbf",
+          400: "#999999",
+          500: "#666666",
+          600: "#4d4d4d",
+          700: "#333333",
+          800: "#1a1a1a",
+          900: "#0a0a0a"
+        }
+      }
+    }
   },
   plugins: [],
 };
